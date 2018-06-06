1b84a9e00ff14422f428b054c38087374191db9b
Arno de Jonge <developeradj@gmail.com>
2018-06-06 21:35:38 +0200

    Feat(browser_locale): add browser locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action { set_locale(get_browser_locale, Settings.languages[:available_languages]) }
+
+    def set_locale(browser_locales, available_locales)
+    I18n.locale = Settings.languages.default_language
+    browser_locales.each do |browser_locale|
+      if available_locales.include?(browser_locale)
+        I18n.locale = browser_locale
+        break
+      end
+    end
+    puts "Avaiable Languages: #{Settings.languages[:available_languages]}\nDefault Language: #{Settings.languages[:default_language]}\nBrowser Preferred Languages: #{browser_locales}\nLocale set to #{I18n.locale.upcase}" unless Rails.env.test?
+    I18n.locale
+  end
+
+  def get_browser_locale
+  	begin
+  		request.env['HTTP_ACCEPT_LANGUAGE'].scan(/[a-z]{2}(?=; || ?=,)/)
+  	rescue
+  		[Settings.languages[:default_language]]
+  	end
+  end
 end
